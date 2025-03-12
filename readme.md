b30a961165e5ba0682e6d31f84cfdadb86b00b84
AronBjartur <abh41@hi.is>
2025-03-12 13:18:15 +0000

    readme og db fyllt

--- readme.md
+++ readme.md
@@ -1,6 +1,33 @@
-algjör grunnur bara
+# Hópverkefni 1 – Vefforritun 2, vor 2025
 
-# Höfundar
+## Uppsetning verkefnisins
 
-Aron Bjartur Hilmarsson - abh41@hi.is - github.com/aronbjartur
-Arnaldur Ólafsson - aro42@hi.is - github.com/Add1Padd1
+   github.com/aronbjartur/v2h1
+
+   nota þessar command línur:
+   npm install
+   npm run setup
+   npm start
+
+   .Env á að vera í rót verkefnis og er .env gefið upp í canvas.
+   DATABASE_URL=postgresql://...
+    JWT_SECRET=...
+    SESSION_SECRET=...
+    TOKEN_LIFETIME=3600
+    BCRYPT_ROUNDS=10
+    PORT=3000
+
+## Test gögn
+
+TODO
+
+## Innskráning fyrir admin stjórnanda
+	•	Notandi: admin
+
+	•	Lykilorð: password123
+
+
+## Höfundar
+    Aron Bjartur Hilmarsson - abh41@hi.is - github.com/aronbjartur
+
+    Arnaldur Ólafsson - aro42@hi.is - github.com/Add1Padd1
--- sql/schema.sql
+++ sql/schema.sql
@@ -1,6 +1,11 @@
 DROP TABLE IF EXISTS transactions;
+DROP TABLE IF EXISTS budgets;
+DROP TABLE IF EXISTS accounts;
+DROP TABLE IF EXISTS payment_methods;
+DROP TABLE IF EXISTS categories;
 DROP TABLE IF EXISTS users;
 
+-- notendur
 CREATE TABLE IF NOT EXISTS users (
   id SERIAL PRIMARY KEY,
   username VARCHAR(255) UNIQUE NOT NULL,
@@ -11,12 +16,44 @@ CREATE TABLE IF NOT EXISTS users (
   updated TIMESTAMP
 );
 
-CREATE TABLE transactions (
+--reikningar, þannig fólk geymt á mismundandi reikningum
+CREATE TABLE IF NOT EXISTS accounts (
   id SERIAL PRIMARY KEY,
   user_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
+  account_name VARCHAR(100) NOT NULL,
+  balance NUMERIC(12,2) DEFAULT 0,
+  created TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+--flokarnir, tegund inngjalda og útgjalda
+CREATE TABLE IF NOT EXISTS categories (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(50) UNIQUE NOT NULL -- eins og matvörur, húsnæði, skólar, bílar eða ehv þannig
+);
+
+--greiðslumáti, penignar eða credit kort eða debit
+CREATE TABLE IF NOT EXISTS payment_methods (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(50) UNIQUE NOT NULL
+);
+
+--færslurnar, inngjöldin og útgjöldin
+CREATE TABLE IF NOT EXISTS transactions (
+  id SERIAL PRIMARY KEY,
+  account_id INTEGER REFERENCES accounts(id) ON DELETE CASCADE,
+  user_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
+  payment_method_id INTEGER REFERENCES payment_methods(id),
   transaction_type VARCHAR(50) NOT NULL, -- inn og utgjöld
-  category VARCHAR(50) NOT NULL, -- eins og matvörur, húsnæði, skólar, bílar eða ehv þannig
+  category VARCHAR(50) NOT NULL references categories(name), -- eins og matvörur, húsnæði, skólar, bílar eða ehv þannig
   amount NUMERIC(10,2) NOT NULL,
-  date DATE NOT NULL,
   description TEXT
+);
+
+--mánaðar takmark annig eyðsla er ekki of mikil
+CREATE TABLE IF NOT EXISTS budgets (
+  id SERIAL PRIMARY KEY,
+  user_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
+  category VARCHAR(50) NOT NULL REFERENCES categories(name),
+  monthly_limit NUMERIC(10,2) NOT NULL,
+  created TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
